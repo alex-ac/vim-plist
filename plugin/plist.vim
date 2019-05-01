ba5606895db6ab3d00ea6ca0dc1d24533c1aa07e
Aleksandr Derbenev <ya.alex-ac@yandex.com>
2019-05-01 10:49:35 +0100

    Add :Plview command.

--- plugin/plist.vim
+++ plugin/plist.vim
@@ -14,3 +14,24 @@ function! GetPlistFold(lnum)
   return '='
 endfunction
 
+let s:plugin_root_dir = fnamemodify(resolve(expand('<sfile>:p')), ':h')
+
+python3 << EOF
+import os
+import sys
+
+import vim
+
+plugin_root_dir = vim.eval('s:plugin_root_dir')
+python_root_dir = os.path.normpath(os.path.join(plugin_root_dir, '..', 'python'))
+
+sys.path.insert(0, python_root_dir)
+print('python path: {}'.format(sys.path))
+import vim_plist
+EOF
+
+function! LoadPlist()
+  python3 vim_plist.load_plist()
+endfunction
+
+command! Plview call LoadPlist()
---
+++ python/vim_plist.py
@@ -0,0 +1,43 @@
+import collections
+import plistlib
+
+import vim
+
+def dump_view(key, node):
+    if isinstance(node, dict):
+        if key is not None:
+            yield key + ':'
+            for k, v in node.items():
+                for line in dump_view(k, v):
+                    yield '  ' + line
+        else:
+            for k, v in node.items():
+                for line in dump_view(k, v):
+                    yield line
+    elif isinstance(node, list):
+        if key is not None:
+            yield key + ':'
+        for v in node:
+            for line in dump_view(None, v):
+                yield '- ' + line
+    else: 
+        if key is not None:
+            yield '{}: {}'.format(key, node)
+        else:
+            yield str(node)
+
+def load_plist():
+    current_buffer = vim.current.buffer
+    filename = current_buffer.name
+    with open(filename, 'rb') as f:
+        data = plistlib.load(f, dict_type=collections.OrderedDict)
+
+    lines = list(dump_view(None, data))
+
+    vim.command('vsplit PLIST_DATA')
+    vim.command('normal! ggdG')
+    vim.command('setlocal ft=plist-data')
+    vim.command('setlocal buftype=nofile')
+
+    view_buffer = vim.current.buffer
+    view_buffer[:] = lines
